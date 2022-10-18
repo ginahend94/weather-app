caa14633467a775b73deb28da23d32d0b7ae2d34
Gina Henderson <gina@ginahenderson.me>
2022-10-18 15:50:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Default to Nashville
+// Grab query
+const locationInput = document.querySelector('.location-input input');
+const locationSubmit = document.querySelector('#submit');
+console.log(locationInput);
+const getQuery = () => locationInput.value;
+locationSubmit.addEventListener('click', ()=>{});
+
+// Call API
+const key = '2db6a02c5ebe70c03c3c00caa4802366';
+
+// Process JSON data
+// Display data
+// Change background and images based on data
+
+// TODO - make cute lofi/anime theme
+// TODO - foreground is window
+// TODO - add list of possible weather conditions
+// TODO - change to f or c based on location
+// TODO - add autocomplete
+// TODO - add direct stream from lofi girl
+// TODO - add mini to-do list app
+// TODO - add news blurb
+// TODO - add pomodoro timer
+ 
