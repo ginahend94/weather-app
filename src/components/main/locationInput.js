865670b4a72f1da7162ed851c595d8fc1d937340
Gina Henderson <gina@ginahenderson.me>
2022-10-20 13:14:51 -0500

    Fix autocomplete

--- src/components/main/locationInput.js
+++ src/components/main/locationInput.js
@@ -1,16 +1,22 @@
-// import autocomplete from '../functions/autocomplete';
+import { getLocation, getWeather } from '../../functions/callOpenWeather';
 
-export default (() => {
-  const container = document.createElement('form');
+(() => {
+  const container = document.createElement('div');
+  const form = document.createElement('form');
   const input = document.createElement('input');
   const submit = document.createElement('button');
+  const autocompleteResponse = document.createElement('div');
   let query;
 
-  container.classList.add('location-input');
+  container.classList.add('location-search');
+  form.classList.add('location-input');
   input.placeholder = 'Enter a location';
   submit.textContent = 'Go';
+  autocompleteResponse.classList.add('autocomplete-container');
+  autocompleteResponse.textContent = 'Autocomple results here';
 
-  container.append(input, submit);
+  container.append(form, autocompleteResponse);
+  form.append(input, submit);
 
   const getInput = () => input.value;
   const setInput = (text) => (input.value = text);
@@ -21,18 +27,149 @@ export default (() => {
     setInput(location.string);
   };
 
-  container.addEventListener('submit', (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(e);
     console.log('submitted');
   });
 
   return {
-    container,
+    form,
     getInput,
     setInput,
     getQuery,
     setQuery,
     setQueryAndInput,
+    autocompleteResponse,
   };
 })();
+
+const locationSearch = (() => {
+  const locationContainer = document.createElement('div');
+
+  const form = (() => {
+    const container = document.createElement('form');
+    const input = document.createElement('input');
+    const submit = document.createElement('button');
+    let query;
+
+    container.classList.add('location-input');
+    input.placeholder = 'Enter a location';
+    submit.textContent = 'Go';
+
+    const getInput = () => input.value;
+    const setInput = (text) => (input.value = text);
+    const getQuery = () => query;
+    const setQuery = (location) => (query = location);
+    const setQueryAndInput = (location) => {
+      setQuery(location);
+      setInput(location.string);
+    };
+
+    container.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (!getQuery()) return;
+      console.log(getQuery());
+      console.log('submitted');
+    });
+    input.addEventListener('input', () => {
+      if (!getInput()) return;
+      // eslint-disable-next-line no-use-before-define
+      autocomplete.start();
+    });
+
+    container.append(input, submit);
+
+    return {
+      container,
+      getInput,
+      getQuery,
+      setQueryAndInput,
+    };
+  })();
+
+  const autocompleteContainer = (() => {
+    const container = document.createElement('div');
+    container.classList.add('autocomplete-container');
+    container.textContent = 'Autocomplete results here';
+    return container;
+  })();
+
+  locationContainer.classList.add('location-search');
+  locationContainer.append(form.container, autocompleteContainer);
+
+  // TODO - add arrow functionality for selecting autocomplete
+  const autocomplete = (() => {
+    let timeout;
+
+    // when timeout ends, show results
+    const showResults = (list) => {
+      const container = autocompleteContainer;
+      // create ul for autocomplete list
+      const ul = document.createElement('ul');
+      ul.classList.add('.autocomplete-list');
+
+      const showContainer = () => (container.style.display = 'block');
+      const hideContainer = () => (container.style.display = 'none');
+
+      // remove old results and add new
+      container.innerHTML = '';
+      container.append(ul);
+
+      // close box if no input
+      if (!form.getInput()) {
+        return hideContainer();
+      }
+
+      // otherwise, show box if closed
+      showContainer();
+
+      list.forEach((item) => {
+        // readable string of location name
+        const string = `${item.name},${item.state ? ` ${item.state},` : ''} ${
+          item.country
+        }`;
+        const itemWithString = {
+          ...item,
+          string,
+        };
+
+        // create li element for each item
+        const li = document.createElement('li');
+        ul.append(li);
+        li.classList.add('autocomplete-item');
+        li.textContent = string;
+        li.dataset.lat = item.lat;
+        li.dataset.lon = item.lon;
+        li.addEventListener('click', () => {
+          form.setQueryAndInput(itemWithString);
+          hideContainer();
+        });
+      });
+      return ul;
+    };
+
+    // on input start timeout
+    const newTimeout = () => {
+      setTimeout(() => {
+        // make api call
+        getLocation(form.getInput()).then((res) => showResults(res));
+      }, 500);
+    };
+
+    // if more input, clear timeout
+    const resetTimeout = () => clearTimeout(timeout);
+
+    // start new timeout
+    const restartTimeout = () => {
+      resetTimeout();
+      timeout = newTimeout();
+    };
+
+    return { start: restartTimeout };
+  })();
+
+  return { container: locationContainer };
+})();
+
+export default locationSearch;
--- src/functions/autocomplete.js
+++ src/functions/autocomplete.js
@@ -1,64 +1,74 @@
 import { getLocation } from './callOpenWeather';
+import locationInput from '../components/main/locationInput';
 
 // TODO - add arrow functionality for selecting autocomplete
+const autocomplete = () => {
+  let timeout;
 
-let timeout;
+  // when timeout ends, show results
+  const showResults = (list) => {
+    // create container and ul for autocomplete list
+    const container = locationInput.autocompleteResponse;
+    const ul = document.createElement('ul');
 
-// when timeout ends, show results
-const showResults = (list) => {
-  // grab container and ul for autocomplete list
-  const container = document.querySelector('.autocomplete-container');
-  const ul = container.querySelector('.autocomplete-list');
-  const showContainer = () => (container.style.display = 'block');
-  const hideContainer = () => (container.style.display = 'none');
-  // remove old results
-  ul.innerHTML = '';
+    container.classList.add('.autocomplete-container');
+    ul.classList.add('.autocomplete-list');
 
-  // close box if no input
-  if (!getInput()) {
-    return hideContainer();
-  }
+    const showContainer = () => (container.style.display = 'block');
+    const hideContainer = () => (container.style.display = 'none');
 
-  // otherwise, show box if closed
-  showContainer();
+    // remove old results
+    ul.innerHTML = '';
+    // close box if no input
+    if (!locationInput.getInput()) {
+      return hideContainer();
+    }
 
-  list.forEach((item) => {
-    // readable string of location name
-    const string = `${item.name},${item.state ? ` ${item.state},` : ''} ${
-      item.country
-    }`;
-    const itemWithString = {
-      ...item,
-      string,
-    };
+    // otherwise, show box if closed
+    showContainer();
 
-    // create li element for each item
-    const li = document.createElement('li');
-    ul.append(li);
-    li.classList.add('autocomplete-item');
-    li.textContent = string;
-    li.dataset.lat = item.lat;
-    li.dataset.lon = item.lon;
-    li.addEventListener('click', () => {
-      setQuery(itemWithString);
-      hideContainer();
+    list.forEach((item) => {
+      // readable string of location name
+      const string = `${item.name},${item.state ? ` ${item.state},` : ''} ${
+        item.country
+      }`;
+      const itemWithString = {
+        ...item,
+        string,
+      };
+
+      // create li element for each item
+      const li = document.createElement('li');
+      ul.append(li);
+      li.classList.add('autocomplete-item');
+      li.textContent = string;
+      li.dataset.lat = item.lat;
+      li.dataset.lon = item.lon;
+      li.addEventListener('click', () => {
+        locationInput.setQuery(itemWithString);
+        hideContainer();
+      });
     });
-  });
-  return list;
-};
-// on input start timeout
-const newTimeout = () => {
-  setTimeout(() => {
-    // make api call
-    getLocation(getInput()).then((res) => showResults(res));
-  }, 500);
-};
-// if more input, clear timeout
-const resetTimeout = () => clearTimeout(timeout);
-// start new timeout
-const restartTimeout = () => {
-  resetTimeout();
-  timeout = newTimeout();
-};
+    return list;
+  };
 
+  // on input start timeout
+  const newTimeout = () => {
+    setTimeout(() => {
+      // make api call
+      getLocation(locationInput.getInput()).then((res) => showResults(res));
+    }, 500);
+  };
+
+  // if more input, clear timeout
+  const resetTimeout = () => clearTimeout(timeout);
+
+  // start new timeout
+  const restartTimeout = () => {
+    resetTimeout();
+    timeout = newTimeout();
+  };
+
+  const container = () => {};
+};
 export default restartTimeout;
