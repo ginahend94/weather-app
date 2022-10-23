60ddc86e736b447f5b437b136ad43c60849b8382
Gina Henderson <gina@ginahenderson.me>
2022-10-22 21:35:36 -0500

    format

--- src/functions/callOpenStreetMap.js
+++ src/functions/callOpenStreetMap.js
@@ -2,12 +2,12 @@ const getLocationName = async (coords) => {
   const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords.lat}&lon=${coords.lon}`;
   const res = await fetch(url);
   const json = await res.json();
-  console.log(json)
+  console.log(json);
   return json;
 };
 
 const getLocation = async (input) => {
-  const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&limit=5`
+  const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&limit=5`;
   try {
     const res = await fetch(url);
     const json = await res.json();
