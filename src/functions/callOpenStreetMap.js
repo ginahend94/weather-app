9b667d2ed3009a868bcceb70b297995cb6974cf8
Gina Henderson <gina@ginahenderson.me>
2022-10-22 21:33:40 -0500

    change location api

--- src/functions/callOpenStreetMap.js
+++ src/functions/callOpenStreetMap.js
@@ -2,7 +2,20 @@ const getLocationName = async (coords) => {
   const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords.lat}&lon=${coords.lon}`;
   const res = await fetch(url);
   const json = await res.json();
+  console.log(json)
   return json;
 };
 
-export default getLocationName;
+const getLocation = async (input) => {
+  const url = `https://nominatim.openstreetmap.org/search?q=${input}&format=json&limit=5`
+  try {
+    const res = await fetch(url);
+    const json = await res.json();
+    console.log(json);
+    return json;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export { getLocation, getLocationName };
--- src/functions/callOpenWeather.js
+++ src/functions/callOpenWeather.js
@@ -1,17 +1,18 @@
-import getLocationName from './callOpenStreetMap';
+import { getLocationName } from './callOpenStreetMap';
 import weatherDisplay, { displayData } from '../components/main/weatherDisplay';
 import confirmModal from '../components/dialog';
 
 const key = '2db6a02c5ebe70c03c3c00caa4802366';
 
 const getLocation = async (input) => {
-  // const url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${key}`;
+  // const url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&appid=${key}&limit=10`;
 
   const url = // TEST
     'https://raw.githubusercontent.com/ginahend94/weather-app/master/src/test/test.json';
   try {
     const res = await fetch(url);
     const json = await res.json();
+    // console.log(json);
     return json;
   } catch (e) {
     console.error(e);
@@ -22,9 +23,9 @@ const getWeather = async (data) => {
   if (!data?.lat || !data?.lon || !data) {
     return console.error('no input');
   }
-  // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=${key}`;
-  const url = // TEST
-     'https://raw.githubusercontent.com/ginahend94/weather-app/master/src/test/test-weather.json'; // TEST
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=${key}`;
+  // const url = // TEST
+  //    'https://raw.githubusercontent.com/ginahend94/weather-app/master/src/test/test-weather.json'; // TEST
   weatherDisplay.clearOutputs();
   // Process JSON data
   try {
@@ -51,7 +52,7 @@ const initialWeather = (pos) => {
 };
 const defaultWeather = () => {
   initialWeather({
-    lat: 36.174465,
+    lat: 36.174465, // nashville, tn
     lon: -86.76796,
   });
 };
@@ -62,7 +63,7 @@ const getUserLocation = () => {
       await confirmModal({
         title: 'Location needed for weather data',
         message: 'Your location is needed to get local weather information. Please refresh the page to allow access.\n(This is optional.)',
-        yes: 'Okay',
+        yes: 'OK',
         hideNo: true,
       });
     }
