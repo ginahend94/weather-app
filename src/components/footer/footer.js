9fc565a534c958dfab8c67aa1b0ac6d38f7bf15d
Gina Henderson <gina@ginahenderson.me>
2022-10-27 19:51:17 -0500

    Add website link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,8 +8,13 @@ export default (() => {
     href: 'https://github.com/ginahend94',
     title: 'GitHub',
   });
+  const web = createLink({
+    text: icon('mdi:web'),
+    href: 'https://ginahenderson.me',
+    title: 'Personal website',
+  });
   credit.innerHTML =
     'Created by <a href="https://ginahenderson.me" target="_blank">Gina Henderson</a>.';
-  container.append(credit, github);
+  container.append(credit, github, web);
   return container;
 })();
