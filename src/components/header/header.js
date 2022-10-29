6382b93b7fe8a0edfe063e88953d3929675ba221
Gina Henderson <gina@ginahenderson.me>
2022-10-29 16:20:28 -0500

    Move clock to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,14 @@
+import clock from '../main/clock';
+
 export default (() => {
   const container = document.createElement('header');
   const title = document.createElement('h1');
   title.textContent = 'weather';
 
-  container.append(title);
+  container.append(
+    title,
+    clock(),
+  );
   return {
     container,
   };
